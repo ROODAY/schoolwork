f5363ff90c5fdfccc13ef671e21e8e43787095b0
ROODAY <rudytheninja@gmail.com>
2014-09-13 17:02:06 -0400

    update

--- apstats/blindjustice/js/main.js
+++ apstats/blindjustice/js/main.js
@@ -13,6 +13,8 @@ $(document).ready(function(){
             .transition().duration(350)
             .call(chart);
 
+    	nv.utils.windowResize(chart.update);
+
       return chart;
     });
     function bdbvdata() {
@@ -35,4 +37,56 @@ $(document).ready(function(){
           }
         ];
     }
+
+    nv.addGraph(function() {
+	    var chart = nv.models.multiBarChart()
+	      .transitionDuration(350)
+	      .reduceXTicks(true)   //If 'false', every single x-axis tick label will be rendered.
+	      .rotateLabels(0)      //Angle to rotate x-axis labels.
+	      .showControls(true)   //Allow user to switch between 'Grouped' and 'Stacked' mode.
+	      .groupSpacing(0.1)    //Distance between each group of bars.
+	    ;
+
+	    d3.select('#bwsentences svg')
+	        .datum(allexecutionsdata())
+	        .call(chart);
+
+	    nv.utils.windowResize(chart.update);
+
+	    return chart;
+	});
+
+	function allexecutionsdata() {
+		return [
+				{
+		      key: 'Black Defendants with Black Victims',
+		      values: [
+			    { x: "Executed", y: 76},
+			    { x: "Not Executed", y: 346}
+			  ]
+		    },
+		    {
+		      key: 'Black Defendants with White Victims',
+		      values: [
+			    { x: "Executed", y: 21},
+			    { x: "Not Executed", y: 78}
+			  ]
+		    },
+		    {
+		      key: 'White Defendants with Black Victims',
+		      values: [
+			    { x: "Executed", y: 1},
+			    { x: "Not Executed", y: 24}
+			  ]
+		    },
+		    {
+		      key: 'White Defendants with White Victims',
+		      values: [
+			    { x: "Executed", y: 17},
+			    { x: "Not Executed", y: 104}
+			  ]
+		    }
+		];
+	}
+
 });
