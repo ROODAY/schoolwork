61bc43d56fed9498c7f7c44756e51dc466a91bef
ROODAY <rudytheninja@gmail.com>
2014-10-15 18:38:05 -0400

    styles

--- ancientciv/xerxes/js/main.js
+++ ancientciv/xerxes/js/main.js
@@ -1,1 +1,10 @@
-
+$(document).ready(function(){
+	$("#navbutton").mouseenter(function(){
+		$(this).css("color", "#ccc");
+		$("nav").css("right", "0vw");
+	});
+	$("nav").mouseleave(function(){
+		$("#navbutton").css("color", "#333");
+		$(this).css("right", "-20vw");
+	});
+});
