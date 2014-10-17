f760e08321fb0787dbfeac8e68127f379d38fe3e
ROODAY <rudytheninja@gmail.com>
2014-10-16 23:18:56 -0400

    finished

--- ancientciv/xerxes/js/main.js
+++ ancientciv/xerxes/js/main.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
+	var once = true;
+	var listener = new window.keypress.Listener();
+	listener.sequence_combo("g o d k i n g", function() {
+		if (once) {
+			once = false;
+			$("#hider").css("margin-top", "0");
+			$("#1").attr("src", $("#1").attr("data-lol"));
+	    	$("#2").attr("src", $("#2").attr("data-lol"));
+	    	$("#3").attr("src", $("#3").attr("data-lol"));
+	    	$("#4").attr("src", $("#4").attr("data-lol"));
+	    	var sparta = new Audio('../img/sparta.wav'); 
+			sparta.volume = 1;
+			sparta.addEventListener('ended', function() {
+			    $("#hider").css("margin-top", "100vh");
+			}, false);
+			sparta.play();
+		} 
+	}, true);
 	$("#navbutton").mouseenter(function(){
 		$(this).css("color", "#ccc");
 		$("nav").css("right", "0vw");
