a5f56598fae9f21b2d6fe67eaf3674541af57272
ROODAY <rudytheninja@gmail.com>
2014-10-16 23:33:25 -0400

    perfect

--- ancientciv/xerxes/js/main.js
+++ ancientciv/xerxes/js/main.js
@@ -1,24 +1,38 @@
 $(document).ready(function(){
-	var once = true;
+	$("#1").attr("data-real", $("#1").attr("src"));
+    	$("#2").attr("data-real", $("#2").attr("src"));
+    	$("#3").attr("data-real", $("#3").attr("src"));
+    	$("#4").attr("data-real", $("#4").attr("src"));
+	var lol = false;
 	var listener = new window.keypress.Listener();
 	listener.sequence_combo("g o d k i n g", function() {
-		if (once) {
-			once = false;
-			$("#hider").css("margin-top", "0");
-			$("#1").attr("src", $("#1").attr("data-lol"));
-	    	$("#2").attr("src", $("#2").attr("data-lol"));
-	    	$("#3").attr("src", $("#3").attr("data-lol"));
-	    	$("#4").attr("src", $("#4").attr("data-lol"));
-	    	var sparta = new Audio('../img/sparta.wav'); 
-			sparta.volume = 1;
-			/*sparta.addEventListener('ended', function() {
-			    $("#hider").css("margin-top", "100vh");
-			}, false);*/
-			sparta.play();
+		$("#hider").css("margin-top", "0");
+		if (!lol) {
+			lol = true;
 			setTimeout(function(){
-				$("#hider").css("margin-top", "100vh");
-			}, 2000);
-		} 
+				$("#1").attr("src", $("#1").attr("data-lol"));
+		    	$("#2").attr("src", $("#2").attr("data-lol"));
+		    	$("#3").attr("src", $("#3").attr("data-lol"));
+		    	$("#4").attr("src", $("#4").attr("data-lol"));
+			}, 500);
+		} else {
+			lol = false;
+			setTimeout(function(){
+				$("#1").attr("src", $("#1").attr("data-real"));
+		    	$("#2").attr("src", $("#2").attr("data-real"));
+		    	$("#3").attr("src", $("#3").attr("data-real"));
+		    	$("#4").attr("src", $("#4").attr("data-real"));
+			}, 500);
+		}
+		var sparta = new Audio('../img/sparta.wav'); 
+		sparta.volume = 1;
+		/*sparta.addEventListener('ended', function() {
+		    $("#hider").css("margin-top", "100vh");
+		}, false);*/
+		sparta.play();
+		setTimeout(function(){
+			$("#hider").css("margin-top", "-100vh");
+		}, 2000);
 	}, true);
 	$("#navbutton").mouseenter(function(){
 		$(this).css("color", "#ccc");
