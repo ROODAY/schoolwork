628f20975e6b2b11a04e3f83246303c0accc308e
ROODAY <rudytheninja@gmail.com>
2014-12-11 21:02:24 -0500

    material design

--- ancientciv/amenhotepiii/js/main.js
+++ ancientciv/amenhotepiii/js/main.js
@@ -1,15 +1,9 @@
 $(document).ready(function(){
 	$("#1").attr("data-real", $("#1").attr("src"));
-<<<<<<< HEAD
-    	$("#2").attr("data-real", $("#2").attr("src"));
-    	$("#3").attr("data-real", $("#3").attr("src"));
-    	$("#4").attr("data-real", $("#4").attr("src"));
-=======
 	$("#2").attr("data-real", $("#2").attr("src"));
 	$("#3").attr("data-real", $("#3").attr("src"));
 	$("#4").attr("data-real", $("#4").attr("src"));
 	$("#5").attr("data-real", $("#5").attr("src"));
->>>>>>> e26ea626b4dc8dab70df78f7f3c096775c1fe9c0
 	var lol = false;
 	var listener = new window.keypress.Listener();
 	listener.sequence_combo("s n e f e r u", function() {
@@ -21,10 +15,7 @@ $(document).ready(function(){
 		    	$("#2").attr("src", $("#2").attr("data-lol"));
 		    	$("#3").attr("src", $("#3").attr("data-lol"));
 		    	$("#4").attr("src", $("#4").attr("data-lol"));
-<<<<<<< HEAD
-=======
 		    	$("#5").attr("src", $("#5").attr("data-lol"));
->>>>>>> e26ea626b4dc8dab70df78f7f3c096775c1fe9c0
 			}, 500);
 		} else {
 			lol = false;
@@ -33,20 +24,9 @@ $(document).ready(function(){
 		    	$("#2").attr("src", $("#2").attr("data-real"));
 		    	$("#3").attr("src", $("#3").attr("data-real"));
 		    	$("#4").attr("src", $("#4").attr("data-real"));
-<<<<<<< HEAD
-			}, 500);
-		}
-		var sparta = new Audio('../img/sparta.wav'); 
-		sparta.volume = 1;
-		/*sparta.addEventListener('ended', function() {
-		    $("#hider").css("margin-top", "100vh");
-		}, false);*/
-		sparta.play();
-=======
 		    	$("#5").attr("src", $("#5").attr("data-real"));
 			}, 500);
 		}
->>>>>>> e26ea626b4dc8dab70df78f7f3c096775c1fe9c0
 		setTimeout(function(){
 			$("#hider").css("margin-top", "-100vh");
 		}, 2000);
